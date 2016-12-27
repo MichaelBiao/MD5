9bc900c91201a809919c5f284a53b7e73393ab55
mabiao5 <mabiao5@wanda.cn>
2016-12-27 09:27:59 +0800

    Initial Commit

---
+++ MD5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MD5
+//
+//  Created by BiaoShu on 2016/12/27.
+//  Copyright © 2016年 BiaoShu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
